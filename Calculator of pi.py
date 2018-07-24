ae51f2b53661b271d1bb03be57eaebfbcbb17d5d
Nick Lu <41639441+nicklutuxun@users.noreply.github.com>
2018-07-24 23:35:00 +0800

    Update Calculator of pi.py

--- Calculator of pi.py
+++ Calculator of pi.py
@@ -7,10 +7,13 @@
 import matplotlib.pyplot as plt
 import time
 
-#n是无限级数展开项数
+#n is the term of Maclaurin series
 print('Hi! This is a program to calculate the approximate value of pi')
-n=int(input( 'n是无限级数展开项数\n'+'Please enter n:'))
+n=int(input('Please enter n:'))
+
+#Time starts
 time_start=time.time()
+
 sum_i=0
 for i in range(1,n+1):
     sum_i=(sum_i+(-1)**(i-1)/(2*i-1))
@@ -24,8 +27,10 @@ plt.title('Calculation of pi', fontsize=24)
 plt.xlabel('Value of n',fontsize=20)
 plt.ylabel('Value of sum',fontsize=20)
 
+#Time ends
 time_end=time.time()
 time=time_end-time_start
+
 print('The approximate value of pi=', end='')
 print(sum)
 
